da7ddeb4ce485b708d449aa70c416802e682aad3
mateoarevaloangel <danielmateoarevaloangel@gmail.com>
2025-02-15 22:14:29 -0500

    Angular app

---
+++ FrontCCL/src/app/board-moderator/board-moderator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-moderator',
+  imports: [],
+  templateUrl: './board-moderator.component.html',
+  styleUrl: './board-moderator.component.css'
+})
+export class BoardModeratorComponent {
+
+}
---
+++ FrontCCL/src/app/board-user/board-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-user',
+  imports: [],
+  templateUrl: './board-user.component.html',
+  styleUrl: './board-user.component.css'
+})
+export class BoardUserComponent {
+
+}
